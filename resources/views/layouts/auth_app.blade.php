f2e6b72a796e14e6c8ceddc82eaf14eb35cbbb0f
umair awan <umair.awan@akhuwat.org.pk>
2023-08-24 09:41:35 +0500

    COMMIT

--- resources/views/layouts/auth_app.blade.php
+++ resources/views/layouts/auth_app.blade.php
@@ -10,7 +10,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Tobacco') }}</title>
+    <title>Tobacco</title>
 
     <script src="{{ asset('js/app.js') }}" defer></script>
 
